8f6865c43b5037dec438d522bcc00b64dd958445
HTFDT <almetov_tag@mail.ru>
2024-06-08 23:16:48 +0500

    added UpdateFolderRequest dto

---
+++ core/Dto/Folder/UpdateFolderRequest.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace core.Dto.Folder;
+
+public record UpdateFolderRequest
+{
+    public required string Title { get; init; }
+    public required IFormFile Image { get; init; }
+}
