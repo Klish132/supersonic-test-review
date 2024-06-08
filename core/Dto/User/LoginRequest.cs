3a1fbd42d319181c66326c77e1380a7464276798
HTFDT <almetov_tag@mail.ru>
2024-06-08 20:00:50 +0500

    added LoginRequest dto

---
+++ core/Dto/User/LoginRequest.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace core.Dto.User;
+
+public record LoginRequest
+{
+    [EmailAddress]
+    public required string Email { get; init; }
+    public required string Password { get; init; }
+}
