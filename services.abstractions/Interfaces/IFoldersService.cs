4adb60be3f0c4e472d1c0c82a0704be2bd27b393
HTFDT <almetov_tag@mail.ru>
2024-06-08 23:22:23 +0500

    added IFoldersService interface

---
+++ services.abstractions/Interfaces/IFoldersService.cs
@@ -0,0 +1,14 @@
+﻿using core.Dto.Folder;
+using core.Dto.Note;
+
+namespace services.abstractions.Interfaces;
+
+public interface IFoldersService
+{
+    public Task<CreateFolderResponse> CreateFolderAsync(Guid ownerId, CreateFolderRequest request);
+    public Task<UpdateFolderResponse> UpdateFolderAsync(Guid ownerId, Guid folderId, UpdateFolderRequest request);
+    public Task<IEnumerable<FolderResponse>> GetAllFoldersAsync(Guid ownerId);
+    public Task<IEnumerable<NoteResponse>> GetFolderNotesAsync(Guid ownerId, Guid folderId, GetFolderNotesRequest request);
+    public Task DeleteFolderAsync(Guid ownerId, Guid folderId);
+    public Task<FolderResponse> GetFolderAsync(Guid ownerId, Guid folderId);
+}
