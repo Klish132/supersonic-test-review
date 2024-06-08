f24cbf908a5757c603adcc392869e94e3fd67417
HTFDT <almetov_tag@mail.ru>
2024-06-08 20:58:30 +0500

    added IRepositoryManager interface

---
+++ domain/Interfaces/IRepositoryManager.cs
@@ -0,0 +1,11 @@
+﻿using domain.Entities;
+
+namespace domain.Interfaces;
+
+public interface IRepositoryManager
+{
+    IUnitOfWork UnitOfWork { get; }
+    IRepository<User> Users { get; }
+    IRepository<Folder> Folders { get; }
+    IRepository<Note> Notes { get; }
+}
