dc89062724c17aacb7b40069096a0ddb5df1e096
HTFDT <almetov_tag@mail.ru>
2024-06-09 00:06:01 +0500

    added Folder entity

---
+++ domain/Entities/Folder.cs
@@ -0,0 +1,14 @@
+﻿using core.Entities.Base;
+using core.Enums;
+
+namespace domain.Entities;
+
+public class Folder : IEntity
+{
+    public Guid Id { get; set; }
+    public required string Title { get; set; }
+    public required string ImagePath { get; set; }
+    
+    public User Owner { get; set; } = null!;
+    public Guid OwnerId { get; set; }
+}
