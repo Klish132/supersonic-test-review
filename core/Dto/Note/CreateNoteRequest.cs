25249682e466938d931fd3ae9baf5547058ba84f
HTFDT <almetov_tag@mail.ru>
2024-06-08 23:34:17 +0500

    added CreateNoteRequest dto

---
+++ core/Dto/Note/CreateNoteRequest.cs
@@ -0,0 +1,12 @@
+﻿using core.Enums;
+
+namespace core.Dto.Note;
+
+public record CreateNoteRequest
+{
+    public required string Title { get; init; }
+    public required string Content { get; init; }
+    public required Significance Significance { get; init; } = Significance.Common;
+    public required bool IsFavorite { get; init; }
+    public Guid? FolderId { get; init; }
+}
