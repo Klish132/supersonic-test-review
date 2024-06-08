8baa5cec175b6a5c84504078da035a3b27ce2da7
HTFDT <almetov_tag@mail.ru>
2024-06-08 23:42:31 +0500

    added UpdateNoteResponse dto

---
+++ core/Dto/Note/UpdateNoteResponse.cs
@@ -0,0 +1,16 @@
+﻿using core.Enums;
+
+namespace core.Dto.Note;
+
+public record UpdateNoteResponse
+{
+    public required Guid NoteId { get; init; }
+    public required string Title { get; init; }
+    public required string Content { get; init; }
+    public required Significance Significance { get; init; }
+    public required bool IsFavorite { get; init; }
+    public required DateTime DateCreated { get; init; }
+    public required DateTime DateModified { get; init; }
+    public required Guid OwnerId { get; init; }
+    public Guid? FolderId { get; init; }
+}
