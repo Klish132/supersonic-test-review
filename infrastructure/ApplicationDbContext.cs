b27ebbaf08d1bd639947cdf5a944ab0058dc4d1c
HTFDT <almetov_tag@mail.ru>
2024-06-08 18:17:12 +0500

    added ApplicationDbContext
    (cherry picked from commit 31ff01ecf8884572bbdf0e39b843f931d0e85002)

---
+++ infrastructure/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using domain.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace infrastructure;
+
+internal class ApplicationDbContext : IdentityDbContext<User, IdentityRole<Guid>, Guid>
+{
+    public DbSet<Note> Addresses { get; set; } = null!;
+    public DbSet<Folder> Carts { get; set; } = null!;
+    
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) :
+        base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+    }
+}
