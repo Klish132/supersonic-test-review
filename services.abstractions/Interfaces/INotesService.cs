c1ef5297f63f969c541a14abcb6587772c2e3a1d
HTFDT <almetov_tag@mail.ru>
2024-06-08 23:46:51 +0500

    added INotesService interface

---
+++ services.abstractions/Interfaces/INotesService.cs
@@ -0,0 +1,12 @@
+﻿using core.Dto.Note;
+
+namespace services.abstractions.Interfaces;
+
+public interface INotesService
+{
+    public Task<CreateNoteResponse> CreateNoteAsync(Guid ownerId, CreateNoteRequest request);
+    public Task<UpdateNoteResponse> UpdateNoteAsync(Guid ownerId, Guid noteId, UpdateNoteRequest request);
+    public Task<IEnumerable<NoteResponse>> GetUserNotesAsync(Guid ownerId, GetNotesRequest request);
+    public Task DeleteNoteAsync(Guid ownerId, Guid noteId);
+    public Task<NoteResponse> GetNoteAsync(Guid ownerId, Guid noteId);
+}
