b2b859d15e37cd00343f4bd3bb16e5a669e1fef7
HTFDT <almetov_tag@mail.ru>
2024-06-08 18:27:28 +0500

    added Notes entity configuration
    (cherry picked from commit ea92deb54d0186847761cee8c2fc13b9b954a022)

---
+++ infrastructure/EntitiesConfigurations/NotesConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace infrastructure.EntitiesConfigurations;
+
+internal class NotesConfiguration : IEntityTypeConfiguration<Note>
+{
+    public void Configure(EntityTypeBuilder<Note> builder)
+    {
+        builder.ToTable("Notes");
+
+        builder.HasOne(e => e.Owner)
+            .WithMany()
+            .HasForeignKey(e => e.OwnerId)
+            .IsRequired();
+        
+        builder.HasIndex(e => e.Title)
+            .IsUnique();
+    }
+}
