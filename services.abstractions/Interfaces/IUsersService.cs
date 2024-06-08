81a346f3b63539f77c06969794adf1478be9b033
HTFDT <almetov_tag@mail.ru>
2024-06-08 20:11:03 +0500

    added IUsersService interface

---
+++ services.abstractions/Interfaces/IUsersService.cs
@@ -0,0 +1,11 @@
+﻿using core.Dto.User;
+
+namespace services.abstractions.Interfaces;
+
+public interface IUsersService
+{
+    Task<CreateUserResponse> CreateUserAsync(CreateUserRequest request);
+    Task<LoginResponse> LoginAsync(LoginRequest request);
+    Task RevokeTokenAsync(Guid userId);
+    Task<RefreshResponse> RefreshTokenAsync(RefreshRequest request);
+}
