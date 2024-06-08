1593c1707cd117d3fa567208e3137cdbcdd52271
HTFDT <almetov_tag@mail.ru>
2024-06-08 19:49:01 +0500

    added User entity

---
+++ domain/Entities/User.cs
@@ -0,0 +1,13 @@
+﻿using core.Entities.Base;
+using Microsoft.AspNetCore.Identity;
+
+namespace domain.Entities;
+
+public class User : IdentityUser<Guid>, IEntity
+{
+    public string? RefreshToken { get; set; }
+    public DateTime? RefreshTokenExpiryDate { get; set; }
+
+    public ICollection<Folder> Folders { get; set; } = null!;
+    public ICollection<Note> Notes { get; set; } = null!;
+}
