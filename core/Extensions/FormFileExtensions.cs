53cb518f34085fb71e2d25c63d2cf41a98471697
HTFDT <almetov_tag@mail.ru>
2024-06-09 17:51:56 +0500

    added FormFileExtensions static extensions class

---
+++ core/Extensions/FormFileExtensions.cs
@@ -0,0 +1,20 @@
+﻿using core.Exceptions.Common;
+using Microsoft.AspNetCore.Http;
+
+namespace core.Extensions;
+
+public static class FormFileExtensions
+{
+    private static readonly HashSet<string> AllowedExtensions = new() { ".jpg", ".jpg", ".png", ".gif" };
+    public static async Task<string> SaveFormImage(this IFormFile file, string savePath)
+    {
+        var ext = Path.GetExtension(file.FileName);
+        if (!AllowedExtensions.Contains(ext))
+            throw new FileTypeForbiddenHttpException();
+        var filename = Path.GetRandomFileName() + ext;
+        var path = Path.Combine(savePath, filename);
+        await using var fileStream = new FileStream(path, FileMode.Create);
+        await file.CopyToAsync(fileStream);
+        return path;
+    }
+}
