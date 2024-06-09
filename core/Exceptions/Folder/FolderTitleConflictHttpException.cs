30d9151753ff261a86f448bbcefca8df432da018
HTFDT <almetov_tag@mail.ru>
2024-06-09 17:49:21 +0500

    added FolderTitleConflictHttpException class

---
+++ core/Exceptions/Folder/FolderTitleConflictHttpException.cs
@@ -0,0 +1,6 @@
+﻿using core.Exceptions.Base;
+
+namespace core.Exceptions.Folder;
+
+public class FolderTitleConflictHttpException(string title) 
+    : ConflictHttpExceptionBase($"Folder with the title \"{title}\" already exists");
