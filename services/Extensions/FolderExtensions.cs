46fa9084c6b7ef056ad2107515a571c9a0d076b7
HTFDT <almetov_tag@mail.ru>
2024-06-09 17:51:32 +0500

    added FolderExtensions static extensions class

---
+++ services/Extensions/FolderExtensions.cs
@@ -0,0 +1,40 @@
+﻿using core.Dto.Folder;
+using domain.Entities;
+
+namespace services.Extensions;
+
+public static class FolderExtensions
+{
+    public static CreateFolderResponse ToCreateResponse(this Folder folder)
+    {
+        return new CreateFolderResponse
+        {
+            FolderId = folder.Id,
+            Title = folder.Title,
+            ImageId = Path.GetFileNameWithoutExtension(folder.ImagePath),
+            OwnerId = folder.OwnerId
+        };
+    }
+
+    public static UpdateFolderResponse ToUpdateResponse(this Folder folder)
+    {
+        return new UpdateFolderResponse
+        {
+            FolderId = folder.Id,
+            Title = folder.Title,
+            ImageId = Path.GetFileNameWithoutExtension(folder.ImagePath),
+            OwnerId = folder.OwnerId
+        };
+    }
+
+    public static FolderResponse ToFolderResponse(this Folder folder)
+    {
+        return new FolderResponse
+        {
+            FolderId = folder.Id,
+            Title = folder.Title,
+            ImageId = Path.GetFileNameWithoutExtension(folder.ImagePath),
+            OwnerId = folder.OwnerId
+        };
+    }
+}
