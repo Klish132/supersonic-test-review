dd59519b437f32ac52bfe0a586cd6934b980505f
HTFDT <almetov_tag@mail.ru>
2024-06-09 16:29:15 +0500

    added UserNotFoundHttpException class

---
+++ core/Exceptions/User/UserNotFoundHttpException.cs
@@ -0,0 +1,14 @@
+﻿using core.Exceptions.Base;
+
+namespace core.Exceptions.User;
+
+public class UserNotFoundHttpException : NotFoundHttpExceptionBase
+{
+    public UserNotFoundHttpException(Guid userId) : base($"User with the id \"{userId}\" not found")
+    {
+    }
+
+    public UserNotFoundHttpException(string email) : base($"User with the email \"{email}\" not found")
+    {
+    }
+}
