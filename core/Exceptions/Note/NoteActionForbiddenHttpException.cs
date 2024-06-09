8c0a4dddd45b073e39d649959208a21ae64a9a6b
HTFDT <almetov_tag@mail.ru>
2024-06-09 18:14:16 +0500

    added NoteActionForbiddenHttpException class

---
+++ core/Exceptions/Note/NoteActionForbiddenHttpException.cs
@@ -0,0 +1,6 @@
+﻿using core.Exceptions.Base;
+
+namespace core.Exceptions.Note;
+
+public class NoteActionForbiddenHttpException(Guid userId, Guid noteId)
+    : ForbiddenHttpExceptionBase($"User with the id \"{userId}\" isn't authorized to perform this action to note with the id \"{noteId}\"");
