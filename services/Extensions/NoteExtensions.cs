f167af9ae8a71264a95962e07a4aabb689230638
HTFDT <almetov_tag@mail.ru>
2024-06-09 17:51:45 +0500

    added NoteExtensions static extensions class

---
+++ services/Extensions/NoteExtensions.cs
@@ -0,0 +1,55 @@
+﻿using core.Dto.Note;
+using domain.Entities;
+
+namespace services.Extensions;
+
+public static class NoteExtensions
+{
+    public static CreateNoteResponse ToCreateResponse(this Note note)
+    {
+        return new CreateNoteResponse
+        {
+            NoteId = note.Id,
+            Title = note.Title,
+            Content = note.Content,
+            Significance = note.Significance,
+            IsFavorite = note.IsFavorite,
+            DateCreated = note.DateCreated,
+            DateModified = note.DateModified,
+            OwnerId = note.OwnerId,
+            FolderId = note.FolderId
+        };
+    }
+
+    public static UpdateNoteResponse ToUpdateResponse(this Note note)
+    {
+        return new UpdateNoteResponse
+        {
+            NoteId = note.Id,
+            Title = note.Title,
+            Content = note.Content,
+            Significance = note.Significance,
+            IsFavorite = note.IsFavorite,
+            DateCreated = note.DateCreated,
+            DateModified = note.DateModified,
+            OwnerId = note.OwnerId,
+            FolderId = note.FolderId
+        };
+    }
+
+    public static NoteResponse ToNoteResponse(this Note note)
+    {
+        return new NoteResponse
+        {
+            NoteId = note.Id,
+            Title = note.Title,
+            Content = note.Content,
+            Significance = note.Significance,
+            IsFavorite = note.IsFavorite,
+            DateCreated = note.DateCreated,
+            DateModified = note.DateModified,
+            OwnerId = note.OwnerId,
+            FolderId = note.FolderId
+        };
+    }
+}
