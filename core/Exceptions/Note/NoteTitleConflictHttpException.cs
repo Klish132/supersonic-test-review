4cf3c0ef1c0ca75b45989c20a4e1296176406564
HTFDT <almetov_tag@mail.ru>
2024-06-09 18:14:07 +0500

    added NoteTitleConflictHttpException class

---
+++ core/Exceptions/Note/NoteTitleConflictHttpException.cs
@@ -0,0 +1,6 @@
+﻿using core.Exceptions.Base;
+
+namespace core.Exceptions.Note;
+
+public class NoteTitleConflictHttpException(string title) 
+    : ConflictHttpExceptionBase($"Note with the title \"{title}\" already exists");
